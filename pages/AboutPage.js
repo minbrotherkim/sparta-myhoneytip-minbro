5e8ae70274b4e141d7eded420cacc4a6c43a2031
Minbro <minbrotherkim@gmail.com>
2022-03-31 00:47:56 +0900

    2주차 끝

---
+++ pages/AboutPage.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+
+export default function AboutPage() {
+  console.disableYellowBox = true;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        HI! 스파르타코딩 앱개발반에 오신 것을 환영합니다!
+      </Text>
+      <View style={styles.introContainer}>
+        <Image
+          source={{
+            uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2FaboutImage.png?alt=media&token=13e1c4f6-b802-4975-9773-e305fc7475c4",
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.introTitleText}>
+          많은 내용을 간결하게 담아내려 노력했습니다!
+        </Text>
+        <Text style={styles.introdescText}>
+          꼭 완주하셔서 꼭 여러분의 것으로 만들어가시길 바랍니다!
+        </Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Instagram</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#1F266A",
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 50,
+  },
+  introContainer: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    height: 500,
+    marginTop: 40,
+    padding: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 30,
+  },
+  introTitleText: {
+    width: 250,
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  introdescText: {
+    width: 250,
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: "orange",
+    borderRadius: 10,
+    width: 200,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+
+    marginTop: 20,
+  },
+  buttonText: {
+    fontSize: 30,
+    color: "white",
+  },
+});
